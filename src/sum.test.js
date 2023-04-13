d61a09e50bc1a1ff7543a6b924e65219ad8146cd
ai58kojima <129715581+ai58kojima@users.noreply.github.com>
2023-04-13 11:03:52 +0900

    letの練習

--- src/sum.test.js
+++ src/sum.test.js
@@ -12,6 +12,20 @@ test('プログラミングコメントの練習',() => {
 test('constの練習',() => {
     const bookTitle ="本";
     const bookPrice =3000;
+    //値の変更はエラー→実行されない　//bookTitle = "まんが"
     expect(bookTitle).toBe("本");
     expect(bookPrice).toBe(3000);
+});
+
+test('letの練習',() => {
+    let bookTitle;
+    expect(bookTitle).toBe(undefined);
+    bookTitle = "本"
+    expect(bookTitle).toBe("本");
+
+    let bookPrice =3000;
+    expect(bookPrice).toBe(3000);
+
+    bookPrice = 5000;
+    expect(bookPrice).toBe(5000);
 });
