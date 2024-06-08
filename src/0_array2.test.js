0022c12a2f5e61cfd818cf2b81b7b14713f67c30
ai58kojima <129715581+ai58kojima@users.noreply.github.com>
2024-06-08 17:44:42 +0900

    文字列(split,join,正規表現,indexOf,includes)

--- src/0_array2.test.js
+++ src/0_array2.test.js
@@ -114,7 +114,7 @@ test("配列の展開,配列をフラット化", () => {
 });
 //▲
 
-//▼2024/05/30 「配列の反復処理から(map, filter)～文字列」
+//▼2024/05/30 「配列の反復処理から(map, filter)～」
 test("反復処理(map, filter)・メソッドチェーン", () => {
   //配列mapメソッド:配列要素を順番にコールバック関数へ渡し、同じ処理を反復的に実行できる
   //1)mapを使って名前の後ろに「くん」「さん」を追加する処理
@@ -163,9 +163,40 @@ test("反復処理(map, filter)・メソッドチェーン", () => {
     });
   expect(Female2).toStrictEqual(["田中さん"]);
 });
+//▲
 
+//▼2024/6/6 「★文字列結合～」
 test("文字列", () => {
   //文字列リテラルルール："" '' ``
   //エスケープシーケンス：決まった特殊文字のこと（`\n` = 改行）
+  // テンプレートリテラル中に${変数名}  例:`Hello ${name}!`
+  //Stringのatメソッド: str.at(-1)); =>文字列の“列”
+  //ビット列の0と1,Unicodeという文字コード,人は10進数で処理し、コンピューターは2進数で処理している
+
+  //1) 文字列を配列へ分解するsplitメソッド →データ型:配列
+  const strings = "あ、い、う".split("、");
+  expect(strings).toStrictEqual(["あ", "い", "う"]);
+
+  //2) 結合して文字列にするjoinメソッド →データ型:文字列
+  const strings2 = ["あ", "い", "う"].join("・");
+  expect(strings2).toStrictEqual("あ・い・う");
+
+  //3)正規表現で.split(/\s+/);　 例)/\s+/は1つ以上のスペースにマッチする正規表現  →データ型:配列
+  const strings3 = "東京-123 港区-456 浅草".split(/[\s|\d|-]+/);
+  expect(strings3).toStrictEqual(["東京", "港区", "浅草"]);
+
+  //4)文字列indexOfメソッド：指定した文字列で検索し、最初に現れたインデックスを返す  →データ型:数値
+  const url = "https://jsprimer.net/basic/string/";
+  const index = url.indexOf("/b");
+  const newUrl = url.slice(index);
+  expect(newUrl).toStrictEqual("/basic/string/");
+
+  //5)真偽値の取得：startsWith(先頭か),endsWith(末尾か),includes(含むか)  →データ型:数値
+  const alphabet = "abcdefg";
+  expect(alphabet.startsWith("abc")).toStrictEqual(true);
+  expect(alphabet.startsWith("c")).toStrictEqual(false);
+  expect(alphabet.endsWith("c")).toStrictEqual(false);
+  expect(alphabet.includes("e")).toStrictEqual(true);
+  expect(alphabet.includes("z")).toStrictEqual(false);
 });
 //▲
