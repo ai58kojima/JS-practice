b894b4ed233098a3faa42bbb9f1709c5a2e77419
ai58kojima <129715581+ai58kojima@users.noreply.github.com>
2024-07-10 13:09:07 +0900

    追加編集

--- src/function2.test.js
+++ src/function2.test.js
@@ -1,17 +1,22 @@
 //▼2024/7/8 「関数とスコープ・クロージャ―～」
 test("クロージャ―の練習", () => {
-  const countUp = () => {
-    let count = 1;
+  //クロージャーを実装する。クロージャーを呼び出すたびに１ずつ増える
+  // クロージャーを２個宣言して、片方は返り値が３まで呼び出し、もう片方は２まで呼び出す
+  function countUp() {
+    // const countUp = () => {
+    let n = 1;
     return () => {
-      count = count + 1;
-      return count;
+      n = n + 1;
+      return n;
     };
-  };
+  }
   const counter1 = countUp();
   const counter2 = countUp();
 
   expect(counter1()).toStrictEqual(2);
   expect(counter1()).toStrictEqual(3);
+  expect(counter1()).toStrictEqual(4);
   expect(counter2()).toStrictEqual(2);
+  expect(counter2()).toStrictEqual(3);
 });
 //▲
