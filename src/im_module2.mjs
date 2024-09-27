71a2eeeb7b168feef720fdafaa31a6592e440f78
ai58kojima <129715581+ai58kojima@users.noreply.github.com>
2024-09-27 13:59:14 +0900

    ECMAScriptモジュール

---
+++ src/im_module2.mjs
@@ -0,0 +1,21 @@
+import { sum, diff } from "./ex_module2.mjs";
+import {
+  double,
+  isEven,
+  remainder as remainderWithN,
+} from "./modules/math.mjs"; // remainder変数をremainderWithNとして名前付エクスポート
+
+// 名前付き import, export を練習してみる
+// ・同一のファイル(.mjsファイル）
+console.log(sum(5, 8)); //13
+console.log(diff(5, 8)); //-3
+
+// ・別フォルダのファイル (.mjsファイル）
+console.log([1, 2, 3, 4, 5].map(double)); //何らかの処理を行い配列を返すメソッド
+console.log([1, 2, 3, 4, 5].filter(isEven).map(double)); //2,4 → 4,8 //条件に合うものだけを取り出すメソッド
+
+const remainder = (num) => {
+  return remainderWithN(num, 2);
+};
+console.log(remainder(4)); //4%2= 2余0 //remainderWithN(5, 2)
+console.log(remainder(7)); //7%2=3余1
---
+++ src/modules/math.mjs
@@ -0,0 +1,11 @@
+export const double = (num) => {
+  return num * 2;
+};
+export const isEven = (num) => {
+  return num % 2 === 0;
+};
+export const remainder = (a, b) => {
+  //余り
+  return a % b;
+};
+// export { double, isEven, remainder as remainderWithN };
